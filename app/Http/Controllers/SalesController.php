de36c19ef0a9ababbe666ef5746d2bd08f9cceef
Marlon Aguirre <marlon.aguirre.dev@gmail.com>
2023-08-27 16:08:12 -0600

    prueba des

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -213,6 +213,7 @@ class SalesController extends Controller
 
         foreach($sale->items as $itm){
               $product = Product::on('sqlsrvchips')->where('Producto','=', $itm->product_id)->get();
+              dd($product);
               $itm->nombre = $product->Descripcion;
             }
        // $sale->items->nombre="sdfds";
