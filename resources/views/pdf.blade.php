0de2ea50367932ba266ab2d769b47f8a06631bf1
Marlon Aguirre <marlon.aguirre.dev@gmail.com>
2023-08-27 17:29:16 -0600

    publish pdf

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pedido</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <style>
+    <link href="{{ asset('asset/css/bootstrap.min.css') }}" rel="stylesheet">
+     <style>
         .img{
             width:200px;
             height: 200px;
