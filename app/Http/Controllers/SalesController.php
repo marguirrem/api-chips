1b8b12ee657cc77270ca580faea431f57c880b5e
Marlon Aguirre <marlon.aguirre.dev@gmail.com>
2023-08-27 18:02:56 -0600

    pdf sin table

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -219,7 +219,7 @@ class SalesController extends Controller
             $itm->nombre = $product[0]->Descripcion;
         }
        
-        $pdf = Pdf::loadView('pdf', ['sale' => $sale,'cliente'=>$client]);
+        $pdf = Pdf::loadView('pdf', ['sale' => $sale,'cliente'=>$client])->setPaper('a4', 'landscape');
         // download PDF file with download method
         //return $pdf->download('pdf_file.pdf');
         //return $pdf->download('hola.pdf');
